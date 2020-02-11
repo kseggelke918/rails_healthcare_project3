1e12da7230ada34766e38fe8fa42f4b465866198
kseggelke918 <katie.seggelke@gmail.com>
2020-02-11 09:35:55 -0700

    created patient_params and doctor_params methods

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,3 +1,10 @@
 class DoctorsController < ApplicationController
+
+
+    private 
+
+    def doctor_params 
+        params.require(:doctor), permit(:name, :specialty, :patient_id)
+    end 
    
 end
--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,3 +1,10 @@
 class PatientsController < ApplicationController
 
+
+
+    private 
+
+    def patient_params 
+        params.require(:patient), permit(:name, :symptoms, :doctor_id)
+    end 
 end
