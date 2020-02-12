adef7c9d1183421061214d88c46e9f8d6d1d6ad8
kseggelke918 <katie.seggelke@gmail.com>
2020-02-12 10:05:10 -0700

    fixed routing error for new form, created before action to find user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,32 @@
 class UsersController < ApplicationController
     # create user - use devise  
+    before_action :find_user
 
     def new 
-        # @user = User.new 
+        @user = User.new 
     end 
 
     def create 
-        user = User.create(user_params)
+        @user = User.create(user_params)
         
-        if user 
-            session[:user_id] = user.id 
-            redirect_to user_path(user)
+        if @user 
+            session[:user_id] = @user.id 
+            redirect_to user_path(@user)
         else 
             redirect_to new_user_path 
         end 
     end 
 
+    def show 
+
+    end 
+
 
     private 
+
+    def find_user 
+        @user = User.find(params[:id])
+    end 
     def user_params 
         params.require(:user).permit(:name, :specialty, :email, :password, :patient_id)
     end
