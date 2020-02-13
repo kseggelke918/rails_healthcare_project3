3cb0f627c6f098d132033b7929d1668e5649f974
kseggelke918 <katie.seggelke@gmail.com>
2020-02-13 16:43:59 -0700

    added views for patients/edit, patients/new, patients/show, and partial

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,6 +1,12 @@
 class PatientsController < ApplicationController
+    before_action :find_patient, except: [:new, :index]
 
     def new 
+        @patient = Patient.new 
+    end 
+
+    def create(patient_params)
+        byebug
     end 
 
     def index 
@@ -9,7 +15,10 @@ class PatientsController < ApplicationController
 
 
     private 
-
+    def find_patient 
+        @patient = Patient.find_by(name: params[:name])
+    end 
+        
     def patient_params 
         params.require(:patient).permit(:name, :symptoms, :doctor_id)
     end 
