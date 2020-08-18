ca9ee4acc1a33f8c5dc50c4a39912bd8ddffc493
kseggelke918 <katie.seggelke@gmail.com>
2020-08-18 11:52:27 -0600

    add functionality to prohibit past appointments

--- app/models/Appointment.rb
+++ app/models/Appointment.rb
@@ -5,5 +5,12 @@ class Appointment < ApplicationRecord
     scope :next_appointment, -> { upcoming_appointments.order(:time).limit(1).first } 
 
     validates :time, uniqueness: { scope: :user, message: "Doctor already has appointment at this time"}
+    validate :time_not_in_past
+
+    def time_not_in_past
+       if time < Time.now 
+            errors.add(:time, "Error - Please enter a future date and time")
+       end 
+    end
 
 end
